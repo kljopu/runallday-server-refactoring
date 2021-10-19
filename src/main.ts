4ffca0cc82b18ab5eaa4b017ed9728c51ade39a4
kljopuu <kljopuu@gmail.com>
2021-10-19 19:00:13 +0900

    feat: Jira 연동 테스트
    - 지라의 이슈와 연동이 잘 되는지
    - pull request를 잘 가져오는지
    
    Ref: #2
    Resolves: HNLP-2 #close

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const appConfig = configService.get('app');
   const PORT = appConfig.port;
+  console.log('jira issue test')
   await app.listen(3000, () => {
     const loggerService = app.get(MyLoggerService);
     loggerService.log(`server listen on ${PORT}`, 'bootstrap');
