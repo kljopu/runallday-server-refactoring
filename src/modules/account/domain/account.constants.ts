930c30c454ceab24e43d3b6ce7befa0ac5e0c723
kljopuu <kljopuu@gmail.com>
2021-10-21 11:39:34 +0900

    feat: Runner Entity 생성
    - Runner Entity 생성
    - DB 테이블 생성
    
    Resolves: #6 #close
    Ref: HNLP-4

---
+++ src/modules/account/domain/account.constants.ts
@@ -0,0 +1,10 @@
+export enum UserGenderEnum {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
+
+export enum UserStateEnum {
+  LEAVE = 'LEAVE', // 회원 탈퇴
+  INACTIVE = 'INACTIVE', // 휴면 상태.
+  REGISTER = 'REGISTER', // 서비스에 완전히 가입됌.
+}
